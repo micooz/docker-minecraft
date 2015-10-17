aab320b4a65c0507641d3290615c2a603329814b
Micooz <micooz@hotmail.com>
2015-10-17 10:30:19 +0800

    Updated: start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 jarfile="/data/minecraft_server.jar"
+propertyfile="/data/server.properties"
 version="1.8.8"
 maxmemory="1G"
 uri="https://s3.amazonaws.com/Minecraft.Download/versions/${version}/minecraft_server.${version}.jar"
@@ -8,5 +9,9 @@ uri="https://s3.amazonaws.com/Minecraft.Download/versions/${version}/minecraft_s
 if [ ! -f $jarfile ]; then
   curl $uri -o $jarfile
 fi
+
+echo "configuring ${propertyfile} ..."
+
+sed -i -e"s/online-mode=.*/online-mode=false" $propertyfile
 
 java -Xmx${maxmemory} -jar $jarfile nogui
