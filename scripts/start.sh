85cde10a3547d830c7ffbd45cd8be9f399c0d71e
Micooz <micooz@hotmail.com>
2015-10-17 13:18:57 +0800

    Updated: start.sh.

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,7 @@
 
 jarfile="/data/minecraft_server.jar"
 propertyfile="/data/server.properties"
+eulafile="/data/eula.txt"
 version="1.8.8"
 maxmemory="1G"
 uri="https://s3.amazonaws.com/Minecraft.Download/versions/${version}/minecraft_server.${version}.jar"
@@ -13,6 +14,7 @@ fi
 echo "configuring ${propertyfile} ..."
 
 sed -i -e"s/online-mode=.*/online-mode=false/" $propertyfile
+sed -i -e"s/eula=.*/eula=true/" $eulafile
 
 # it's necessary for java running at correct ENV
 cd /data && java -Xmx${maxmemory} -jar minecraft_server.jar nogui
